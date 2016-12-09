771214a14609967fc5291b1159f4948a7b423a96
marios papamanolis <marioswwwe@yahoo.gr>
2016-12-09 12:01:55 +0000

    installed devise gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-https://aqueous-wave-91200.herokuapp.com/ 
+
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
